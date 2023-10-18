fd726c08c0e542f44573d03d4bcc452921b3470b
R-U-Bharti <kumarrubharti.rtcit@gmail.com>
2023-10-18 13:15:01 +0530

    changes

--- src/Components/Pages/NaxatraComponents/Home/HomeIndex.jsx
+++ src/Components/Pages/NaxatraComponents/Home/HomeIndex.jsx
@@ -4,6 +4,9 @@ import NewsCategoriesIndex from '../NewsCategories/NewsCategoriesIndex'
 import BreakingNewsIndex from '../BreakinNews/BreakingNewsIndex'
 import HomeLayout from './HomeLayout'
 import Footerlayout from '../Footer/Footerlayout'
+import footerImage1 from '@/Components/assets/footerimg1.jpg'
+import footerimg2 from '@/Components/assets/footerimg2 (1).jpeg'
+import footerimg3 from '@/Components/assets/footerimg3(2).jpeg'
 
 const HomeIndex = () => {
 
@@ -36,7 +39,7 @@ const HomeIndex = () => {
 
   const data = {
     bigNews: {
-      image: "https://naxatranewshindi.com/wp-content/uploads/2023/10/BAKRI-BAZAR-357x210.jpg",
+      image: footerImage1,
       heading: "Durga Puja 2023: चक्रव्यूह को पार कर श्रद्धालु मां का कर पाएंगे दर्शन, बकरी बाजार में दिखेगा भव्य पूजा पंडाल",
       content: "रांची : राजधानी रांची में दुर्गा पूजा को लेकर काफी धूम देखा जा रहा है. कई भव्य पंडाल बन रहे हैं जो लोगों को काफी	",
       place: "Kanhaiya Taleja",
@@ -45,52 +48,52 @@ const HomeIndex = () => {
 
     smallNews: [
       {
-        image: "https://naxatranewshindi.com/wp-content/uploads/2023/10/BAKRI-BAZAR-210x136.jpg",
+        image: footerimg2,
         heading: "Durga Puja 2023: चक्रव्यूह को पार कर श्रद्धालु मां का कर",
         date: "Oct., 2023",
       },
       {
-        image: "https://naxatranewshindi.com/wp-content/uploads/2023/10/babulal-marandi-86x64.jpeg",
+        image: footerimg3,
         heading: "रांची के हरमू मैदान में 28 अक्टूबर को बीजेपी की संकल्प ",
         date: "Oct., 2023",
       },
       {
-        image: "https://naxatranewshindi.com/wp-content/uploads/2023/10/khatia-sahibganj-86x64.jpeg",
+        image: footerimg2,
         heading: "साहेबगंज : खटिया पर ढोकर 12 किमी की दूरी तय कर मरीजों को",
         date: "Oct., 2023",
       },
       {
-        image: "https://naxatranewshindi.com/wp-content/uploads/2023/10/Jharkhand-Breaking-News-1-86x64.jpeg",
+        image: footerimg3,
         heading: "Jharkhand Breaking News Live: हजारीबाग के लौटवा डैम में ",
         date: "Oct., 2023",
       },
       {
-        image: "https://naxatranewshindi.com/wp-content/uploads/2023/10/Hemant-Soren-1-86x64.jpeg",
+        image: footerimg2,
         heading: "सीएम हेमंत सोरेन ने 827 शिक्षकों को सौंपा नियुक्ति पत्र",
         date: "Oct., 2023",
       },
       {
-        image: "https://naxatranewshindi.com/wp-content/uploads/2023/10/Babulal-marandi-1-86x64.jpg",
+        image: footerimg3,
         heading: "बाबूलाल मरांडी का हेमंत सोरेन पर निशाना, पूर्व आईएएस विजय",
         date: "Oct., 2023",
       },
       {
-        image: "https://naxatranewshindi.com/wp-content/uploads/2023/10/Amar-JP-Patel-86x64.jpg",
+        image: footerimg2,
         heading: "Jharkhand Breaking News Live: सीएम हेमंत सोरेन ने 827",
         date: "Oct., 2023",
       },
       {
-        image: "https://naxatranewshindi.com/wp-content/uploads/2023/10/WhatsApp-Image-2023-10-15-at-6.38.22-PM-86x64.jpeg",
+        image: footerimg3,
         heading: "अमर कुमार बाउरी बने झारखंड बीजेपी विधायक दल के नेता, जेपी",
         date: "Oct., 2023",
       },
       {
-        image: "https://naxatranewshindi.com/wp-content/uploads/2023/09/WhatsApp-Image-2023-09-28-at-3.07.09-PM-86x64.jpeg",
+        image: footerimg2,
         heading: "बीजेपी की संकल्प यात्रा के समापन पर चर्चा, रांची में",
         date: "Oct., 2023",
       },
       {
-        image: "https://naxatranewshindi.com/wp-content/uploads/2023/10/BJP-Ranchi-86x64.jpeg",
+        image: footerimg3,
         heading: "विनिता घोष वतन वापस आई, इजरायल में युद्ध के बीच फंसी थी",
         date: "Oct., 2023",
       },
