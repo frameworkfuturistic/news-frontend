074ed2069e884ce000f255dd749c81bf2fe40331
R-U-Bharti <kumarrubharti.rtcit@gmail.com>
2023-10-25 19:22:40 +0530

    changes

--- src/Components/Pages/Layouts/Components/ClusterComponent.jsx
+++ src/Components/Pages/Layouts/Components/ClusterComponent.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import VideoIndex from '../../NaxatraComponents/Home/VideoIndex'
 
-const ClusterComponent = ({ data = [], split = '1', index='0' }) => {
+const ClusterComponent = (props) => {
 
     const [width, setWidth] = useState('w-full md:w-full')
     const [container, setContainer] = useState('w-full')
@@ -10,7 +10,7 @@ const ClusterComponent = ({ data = [], split = '1', index='0' }) => {
     const navigate = useNavigate()
 
     useEffect(() => {
-        switch (split) {
+        switch (props?.split) {
             case '1': {
                 setWidth('w-full md:w-[calc(100%/1.2)]')
                 setContainer('w-[100%*1]')
@@ -31,14 +31,18 @@ const ClusterComponent = ({ data = [], split = '1', index='0' }) => {
                 setWidth('w-full md:w-[calc(100%/5.2)]')
                 setContainer('w-[100%*5]')
             } break;
+            default : {
+                setWidth('w-full md:w-[calc(100%/3.2)]')
+                setContainer('w-[100%*3]')
+            } break;
         }
-    }, [split])
+    }, [props?.split])
 
 
     return (
         <div className={`${container} h-full flex flex-wrap gap-4 overflow-x-auto justify-between`}>
             {
-                data?.slice(1, parseInt(split) + 1)?.map((elem, index) =>
+                Array.isArray(props?.data) && props?.data?.slice(1, parseInt(props?.split || '3') + 1)?.map((elem, index) =>
                     <>
                         <div className={`${width} grid grid-cols-12 border-t border-gray-300 p-2`} key={index}>
 
@@ -50,7 +54,7 @@ const ClusterComponent = ({ data = [], split = '1', index='0' }) => {
                                 <img src={elem?.source} alt="" srcset="" className='col-span-4 object-contain bg-contain h-16' />
                             }
                             <div className="col-span-8">
-                                <h1 className='hover:text-red-500 text-ellipsis line-clamp-3 font-semibold text-gray-700 text-sm cursor-pointer' onClick={() => navigate(`/news-details/${elem?.id}/${index}`)}>
+                                <h1 className='hover:text-red-500 text-ellipsis line-clamp-3 font-semibold text-gray-700 text-sm cursor-pointer' onClick={() => navigate(`/news-details/${elem?.id}/${props?.index}`)}>
                                     {elem?.heading}
                                 </h1>
                             </div>
