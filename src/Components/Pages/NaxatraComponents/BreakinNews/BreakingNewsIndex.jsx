840a1b62d4811ba53eb0ee88deb21fc049d21115
swarajlinda <swarajlinda2017@gmail.com>
2023-10-27 00:47:52 +0530

    vchanges

--- src/Components/Pages/NaxatraComponents/BreakinNews/BreakingNewsIndex.jsx
+++ src/Components/Pages/NaxatraComponents/BreakinNews/BreakingNewsIndex.jsx
@@ -10,7 +10,13 @@ const BreakingNewsIndex = (props) => {
         <div className={`max-w-[${props?.wpx}] bg-red-600 h-full w-full flex justify-between px-4 py-1 rounded-full font-semibold`} >
             <div className='font-semibold md:text-base text-xs flex'>
             <span className='uppercase italic border-r-2 pr-1 md:mr-2 w-[15%]'>Breaking News</span>
-            <Marquee style={{zIndex: 0}} className='pl-1 md:pl-1 z-0'>राज्यसभा में भी महिला आरक्षण विधेयक पारित, महिला सांसदों ने PM मोदी के साथ मनाया जश्न</Marquee>
+            <Marquee style={{zIndex: 0}} className='pl-1 md:pl-1 z-0'>रांची में वुमेंस एशियन हॉकी चैंपियनशिप ट्रॉफी का गुरुवार से हो रहा आगाज
+झारखंड प्रशासनिक सेवा के 36 अधिकारियों का हुआ तबादला
+बीजेपी के राष्ट्रीय अध्यक्ष जेपी नड्डा 28 अक्टूबर को आएंगे रांची
+पूर्व सीएम रघुवर दास बीजेपी के सभी पदों से देंगे इस्तीफा, ओडिशा के राज्यपाल की लेंगे शपथ
+झारखंड में बढ़ने लगी ठंड, और गिरेगा पारा
+अयोध्या पहुंची बॉलीवुड की एक्ट्रेस कंगना रनौत, रामलला की दर्शन की
+चुनाव आयोग ने एक्टर राजकुमार राव को बनाया नेशनल आईकन</Marquee>
             </div>
 
             <div className='flex items-center' onClick={() => props?.bClose(false)}>
