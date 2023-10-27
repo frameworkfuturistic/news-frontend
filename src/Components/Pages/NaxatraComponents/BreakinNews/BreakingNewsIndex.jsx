54e266d74868bcff2c85f356665304fbd70e6e62
swarajlinda <swarajlinda2017@gmail.com>
2023-10-27 09:22:30 +0530

    topnews

--- src/Components/Pages/NaxatraComponents/BreakinNews/BreakingNewsIndex.jsx
+++ src/Components/Pages/NaxatraComponents/BreakinNews/BreakingNewsIndex.jsx
@@ -10,9 +10,9 @@ const BreakingNewsIndex = (props) => {
         <div className={`max-w-[${props?.wpx}] bg-red-600 h-full w-full flex justify-between px-4 py-1 rounded-full font-semibold`} >
             <div className='font-semibold md:text-base text-xs flex'>
             <span className='uppercase italic border-r-2 pr-1 md:mr-2 w-[15%]'>Breaking News</span>
-            <Marquee style={{zIndex: 0}} className='pl-1 md:pl-1 z-0'>रांची में वुमेंस एशियन हॉकी चैंपियनशिप ट्रॉफी का गुरुवार से हो रहा आगाज
-झारखंड प्रशासनिक सेवा के 36 अधिकारियों का हुआ तबादला
-बीजेपी के राष्ट्रीय अध्यक्ष जेपी नड्डा 28 अक्टूबर को आएंगे रांची
+            <Marquee style={{zIndex: 0}} className='pl-1 md:pl-1 z-0'>रांची में वुमेंस एशियन हॉकी चैंपियनशिप ट्रॉफी का गुरुवार से हो रहा आगाज ,
+झारखंड प्रशासनिक सेवा के 36 अधिकारियों का हुआ तबादला  ,
+बीजेपी के राष्ट्रीय अध्यक्ष जेपी नड्डा 28 अक्टूबर को आएंगे रांची ,
 पूर्व सीएम रघुवर दास बीजेपी के सभी पदों से देंगे इस्तीफा, ओडिशा के राज्यपाल की लेंगे शपथ
 झारखंड में बढ़ने लगी ठंड, और गिरेगा पारा
 अयोध्या पहुंची बॉलीवुड की एक्ट्रेस कंगना रनौत, रामलला की दर्शन की
